ffb6bdc48475f1763a1a166f3a6b3efbd73abbcb
draco1513 <51796955+draco1513@users.noreply.github.com>
2023-07-17 23:56:28 -0500

    haaxz

--- src/main/java/com/reporteriesgo/service/ReporteRiesgoService.java
+++ src/main/java/com/reporteriesgo/service/ReporteRiesgoService.java
@@ -4,12 +4,8 @@ import com.reporteriesgo.persisrence.entity.ReporteRiesgo;
 import com.reporteriesgo.persisrence.repository.ReporteRiesgoRepository;
 import com.reporteriesgo.service.dto.ReporteRiesgoDto;
 import jakarta.transaction.Transactional;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.ResponseEntity;
-import org.springframework.jdbc.core.BeanPropertyRowMapper;
+
 import org.springframework.stereotype.Service;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 
 import java.util.Date;
 import java.util.List;
